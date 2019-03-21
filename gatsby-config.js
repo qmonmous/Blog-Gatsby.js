de0727a241b55064bd882e7f8320ccb2426c7654
qmonmous <q.monmousseau@gmail.com>
2019-03-21 16:26:55 +0100

    ...

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
   siteMetadata: {
     title: 'A blog by Quentin Monmousseau',
     description: 'Here I write about Data Science, Cognitive Science through UX/UI design, and Sports Science.'
-    siteUrl: 'upbeat-murdock-d33ac7.netlify.com', // full path to blog - no ending slash
+    siteUrl: 'https://upbeat-murdock-d33ac7.netlify.com', // full path to blog - no ending slash
   },
   mapping: {
     'MarkdownRemark.frontmatter.author': 'AuthorYaml',
--- src/website-config.ts
+++ src/website-config.ts
@@ -1,19 +1,19 @@
 export interface WebsiteConfig {
-  title: string;
-  description: string;
-  coverImage: string;
-  logo: string;
+  title: 'string';
+  description: 'string';
+  coverImage: '';
+  logo: '';
   /**
    * Specifying a valid BCP 47 language helps screen readers announce text properly.
    * See: https://dequeuniversity.com/rules/axe/2.2/valid-lang
    */
-  lang: string;
+  lang: 'en';
   /**
    * blog full path, no ending slash!
    */
-  siteUrl: string;
-  facebook?: string;
-  twitter?: string;
+  siteUrl: 'https://upbeat-murdock-d33ac7.netlify.com';
+  facebook?: '';
+  twitter?: '';
   /**
    * hide or show all email subscribe boxes
    */
@@ -34,9 +34,9 @@ const config: WebsiteConfig = {
   coverImage: 'img/blog-cover.jpg',
   logo: 'img/ghost-logo.png',
   lang: 'en',
-  siteUrl: 'https://gatsby-casper.netlify.com',
+  siteUrl: 'https://upbeat-murdock-d33ac7.netlify.com',
   facebook: 'https://www.facebook.com/ghost',
-  twitter: 'https://twitter.com/tryghost',
+  twitter: 'https://upbeat-murdock-d33ac7.netlify.com',
   showSubscribe: true,
   mailchimpAction: 'https://twitter.us19.list-manage.com/subscribe/post?u=a89b6987ac248c81b0b7f3a0f&amp;id=7d777b7d75',
   mailchimpName: 'b_a89b6987ac248c81b0b7f3a0f_7d777b7d75',
