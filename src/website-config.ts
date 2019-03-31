dfa3014f9017455e9fceeffef5af2b1359c50d11
qmonmous <q.monmousseau@gmail.com>
2019-03-31 07:03:11 +0200

    fix twitter link

--- src/website-config.ts
+++ src/website-config.ts
@@ -37,7 +37,7 @@ const config: WebsiteConfig = {
   siteUrl: 'https://gatsby-casper.netlify.com',
   github: 'https://github.com/qmonmous',
   linkedin: 'https://www.linkedin.com/in/quentin-monmousseau',
-  twitter: 'https://twitter.com/tryghost',
+  twitter: 'https://twitter.com/QMonmousseau',
   showSubscribe: true,
   mailchimpAction: 'https://twitter.us19.list-manage.com/subscribe/post?u=a89b6987ac248c81b0b7f3a0f&amp;id=7d777b7d75',
   mailchimpName: 'b_a89b6987ac248c81b0b7f3a0f_7d777b7d75',
