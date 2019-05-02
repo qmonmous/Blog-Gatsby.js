69d7ee4b499525fb1059bfb396db50d650dcaeb5
qmonmous <q.monmousseau@gmail.com>
2019-05-02 17:06:11 +0200

    put author as index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,207 +1,315 @@
 import { graphql } from 'gatsby';
-import * as React from 'react';
+import React from 'react';
+import styled from '@emotion/styled';
 import { css } from '@emotion/core';
-import Helmet from 'react-helmet';
 
 import Footer from '../components/Footer';
 import SiteNav from '../components/header/SiteNav';
 import PostCard from '../components/PostCard';
 import Wrapper from '../components/Wrapper';
 import IndexLayout from '../layouts';
-import config from '../website-config';
 import {
+  AuthorProfileImage,
   inner,
   outer,
   PostFeed,
   PostFeedRaise,
-  SiteDescription,
   SiteHeader,
   SiteHeaderContent,
-  SiteMain,
   SiteTitle,
+  SiteMain,
+  SocialLink,
 } from '../styles/shared';
-import { PageContext } from '../templates/post';
+import { PageContext } from './post';
+import Helmet from 'react-helmet';
+import config from '../website-config';
+import Website from '../components/icons/website';
+import Linkedin from '../components/icons/linkedin';
+import Twitter from '../components/icons/twitter';
+import Github from '../components/icons/github';
 
-const HomePosts = css`
-  @media (min-width: 795px) {
-    .post-card:nth-of-type(6n + 1):not(.no-image) {
-      flex: 1 1 100%;
-      flex-direction: row;
-    }
-
-    .post-card:nth-of-type(6n + 1):not(.no-image) .post-card-image-link {
-      position: relative;
-      flex: 1 1 auto;
-      border-radius: 5px 0 0 5px;
-    }
-
-    .post-card:nth-of-type(6n + 1):not(.no-image) .post-card-image {
-      position: absolute;
-      width: 100%;
-      height: 100%;
-    }
-
-    .post-card:nth-of-type(6n + 1):not(.no-image) .post-card-content {
-      flex: 0 1 357px;
-    }
-
-    .post-card:nth-of-type(6n + 1):not(.no-image) h2 {
-      font-size: 2.6rem;
-    }
-
-    .post-card:nth-of-type(6n + 1):not(.no-image) p {
-      font-size: 1.8rem;
-      line-height: 1.55em;
-    }
-
-    .post-card:nth-of-type(6n + 1):not(.no-image) .post-card-content-link {
-      padding: 30px 40px 0;
-    }
-
-    .post-card:nth-of-type(6n + 1):not(.no-image) .post-card-meta {
-      padding: 0 40px 30px;
-    }
+const HiddenMobile = css`
+  @media (max-width: 500px) {
+    display: none;
   }
 `;
 
-export interface IndexProps {
+const AuthorMeta = styled.div`
+  z-index: 10;
+  flex-shrink: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 0 10px 0;
+  font-family: Georgia, serif;
+  font-style: italic;
+`;
+
+const AuthorBio = styled.h2`
+  z-index: 10;
+  flex-shrink: 0;
+  margin: 5px 0 10px 0;
+  max-width: 600px;
+  font-size: 2rem;
+  line-height: 1.3em;
+  font-weight: 300;
+  letter-spacing: 0.5px;
+  opacity: 0.8;
+`;
+
+const Bull = styled.span`
+  display: inline-block;
+  margin: 0 12px;
+  opacity: 0.5;
+`;
+
+const AuthorProfileBioImage = css`
+  z-index: 10;
+  flex-shrink: 0;
+  margin: 0 0 20px 0;
+  width: 100px;
+  height: 100px;
+  box-shadow: rgba(255, 255, 255, 0.9) 0 0 0 3px;
+`;
+
+interface AuthorTemplateProps {
+  pathContext: {
+    slug: string;
+  };
+  pageContext: {
+    author: string;
+  };
   data: {
     logo: {
-      childImageSharp: {
-        fixed: any;
-      };
-    };
-    header: {
       childImageSharp: {
         fluid: any;
       };
     };
     allMarkdownRemark: {
+      totalCount: number;
       edges: {
         node: PageContext;
       }[];
     };
+    authorYaml: {
+      id: string;
+      linkedin?: string;
+      twitter?: string;
+      github?: string;
+      location?: string;
+      profile_image?: {
+        childImageSharp: {
+          fluid: any;
+        };
+      };
+      bio?: string;
+      avatar: {
+        childImageSharp: {
+          fluid: any;
+        };
+      };
+    };
   };
 }
 
-const IndexPage: React.FunctionComponent<IndexProps> = props => {
-  const width = props.data.header.childImageSharp.fluid.sizes.split(', ')[1].split('px')[0];
-  const height = String(Number(width) / props.data.header.childImageSharp.fluid.aspectRatio);
+const Author: React.FunctionComponent<AuthorTemplateProps> = props => {
+  const author = props.data.authorYaml;
+  
+  const edges = props.data.allMarkdownRemark.edges.filter(
+    (edge) => {
+      const isDraft = (edge.node.frontmatter.draft !== true ||
+        process.env.NODE_ENV === 'development')
+      return isDraft && edge.node.frontmatter.author && edge.node.frontmatter.author.id === author.id
+    }
+  );
+  const totalCount = edges.length;
+
   return (
-    <IndexLayout css={HomePosts}>
+    <IndexLayout>
       <Helmet>
         <html lang={config.lang} />
-        <title>{config.title}</title>
-        <meta name="description" content={config.description} />
+        <title>
+          {author.id} - {config.title}
+        </title>
+        <meta name="description" content={author.bio} />
         <meta property="og:site_name" content={config.title} />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content={config.title} />
-        <meta property="og:description" content={config.description} />
-        <meta property="og:url" content={config.siteUrl} />
-        <meta
-          property="og:image"
-          content={`${config.siteUrl}${props.data.header.childImageSharp.fluid.src}`}
-        />
-        {config.facebook && <meta property="article:publisher" content={config.facebook} />}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content={config.title} />
-        <meta name="twitter:description" content={config.description} />
-        <meta name="twitter:url" content={config.siteUrl} />
-        <meta
-          name="twitter:image"
-          content={`${config.siteUrl}${props.data.header.childImageSharp.fluid.src}`}
-        />
+        <meta property="og:type" content="profile" />
+        <meta property="og:title" content={`${author.id} - ${config.title}`} />
+        <meta property="og:url" content={config.siteUrl + props.pathContext.slug} />
+        <meta property="article:publisher" content="https://www.facebook.com/ghost" />
+        <meta property="article:author" content="https://www.facebook.com/ghost" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content={`${author.id} - ${config.title}`} />
+        <meta name="twitter:url" content={config.siteUrl + props.pathContext.slug} />
         {config.twitter && (
           <meta
             name="twitter:site"
             content={`@${config.twitter.split('https://twitter.com/')[1]}`}
           />
         )}
-        <meta property="og:image:width" content={width} />
-        <meta property="og:image:height" content={height} />
+        {config.twitter && (
+          <meta
+            name="twitter:creator"
+            content={`@${config.twitter.split('https://twitter.com/')[1]}`}
+          />
+        )}
       </Helmet>
       <Wrapper>
         <header
+          className="no-cover"
           css={[outer, SiteHeader]}
           style={{
-            backgroundImage: `url('${props.data.header.childImageSharp.fluid.src}')`,
+            backgroundImage: author.profile_image
+              ? `url(${author.profile_image.childImageSharp.fluid.src})`
+              : '',
           }}
         >
           <div css={inner}>
+            <SiteNav isHome={false} />
             <SiteHeaderContent>
-              <SiteTitle>
-                {props.data.logo ? (
-                  <img
-                    style={{ maxHeight: '45px' }}
-                    src={props.data.logo.childImageSharp.fixed.src}
-                    alt={config.title}
-                  />
-                ) : (
-                  config.title
+              <img
+                css={[AuthorProfileImage, AuthorProfileBioImage]}
+                src={props.data.authorYaml.avatar.childImageSharp.fluid.src}
+                alt={author.id}
+              />
+              <SiteTitle>{author.id}</SiteTitle>
+              {author.bio && <AuthorBio>{author.bio}</AuthorBio>}
+              <AuthorMeta>
+                {author.location && (
+                  <div css={HiddenMobile}>
+                    {author.location} <Bull>&bull;</Bull>
+                  </div>
                 )}
-              </SiteTitle>
-              <SiteDescription>{config.description}</SiteDescription>
+                <div css={HiddenMobile}>
+                  {totalCount > 1 && `${totalCount} posts`}
+                  {totalCount === 1 && `1 post`}
+                  {totalCount === 0 && `No posts`} <Bull>•</Bull>
+                </div>
+                {author.website && (
+                  <div>
+                    <a
+                      className="social-link-wb"
+                      css={SocialLink}
+                      href={author.website}
+                      title="Website"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Website />
+                    </a>
+                  </div>
+                )}
+                {author.twitter && (
+                  <a
+                    className="social-link-tw"
+                    css={SocialLink}
+                    href={author.twitter}
+                    title="Twitter"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <Twitter />
+                  </a>
+                )}
+                {author.linkedin && (
+                  <a
+                    className="social-link-tw"
+                    css={SocialLink}
+                    href={author.linkedin}
+                    title="Linkedin"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <Linkedin />
+                  </a>
+                )}
+                {author.github && (
+                  <a
+                    className="social-link-tw"
+                    css={SocialLink}
+                    href={author.github}
+                    title="Github"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <Github />
+                  </a>
+                )}
+                {/* TODO: RSS for author */}
+                {/* <a
+                  css={SocialLink} className="social-link-rss"
+                  href="https://feedly.com/i/subscription/feed/https://demo.ghost.io/author/ghost/rss/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    style={{ height: '1.9rem' }}
+                  >
+                    <circle cx="6.18" cy="17.82" r="2.18" />
+                    <path d="M4 4.44v2.83c7.03 0 12.73 5.7 12.73 12.73h2.83c0-8.59-6.97-15.56-15.56-15.56zm0 5.66v2.83c3.9 0 7.07 3.17 7.07 7.07h2.83c0-5.47-4.43-9.9-9.9-9.9z" />
+                  </svg>
+                </a> */}
+              </AuthorMeta>
             </SiteHeaderContent>
-            <SiteNav isHome={true} />
           </div>
         </header>
         <main id="site-main" css={[SiteMain, outer]}>
           <div css={inner}>
             <div css={[PostFeed, PostFeedRaise]}>
-              {props.data.allMarkdownRemark.edges.map(post => {
-                // filter out drafts in production
-                return (
-                  (post.node.frontmatter.draft !== true ||
-                    process.env.NODE_ENV !== 'production') && (
-                    <PostCard key={post.node.fields.slug} post={post.node} />
-                  )
-                );
+              {edges.map(({ node }) => {
+                return <PostCard key={node.fields.slug} post={node} />;
               })}
             </div>
           </div>
         </main>
-        {props.children}
-
         <Footer />
       </Wrapper>
     </IndexLayout>
   );
 };
 
-export default IndexPage;
+export default Author;
 
 export const pageQuery = graphql`
-  query {
-    logo: file(relativePath: { eq: "img/ghost-logo.png" }) {
-      childImageSharp {
-        # Specify the image processing specifications right in the query.
-        # Makes it trivial to update as your page's design changes.
-        fixed {
-          ...GatsbyImageSharpFixed
+  query($author: String) {
+    authorYaml(id: { eq: $author }) {
+      id
+      website
+      twitter
+      bio
+      linkedin
+      github
+      location
+      profile_image {
+        childImageSharp {
+          fluid(maxWidth: 3720) {
+            ...GatsbyImageSharpFluid
+          }
         }
       }
-    }
-    header: file(relativePath: { eq: "img/blog-cover.jpg" }) {
-      childImageSharp {
-        # Specify the image processing specifications right in the query.
-        # Makes it trivial to update as your page's design changes.
-        fluid(maxWidth: 2000) {
-          ...GatsbyImageSharpFluid
+      avatar {
+        childImageSharp {
+          fluid(maxWidth: 200) {
+            ...GatsbyImageSharpFluid
+          }
         }
       }
     }
     allMarkdownRemark(
-      sort: { fields: [frontmatter___date], order: DESC },
       filter: { frontmatter: { draft: { ne: true } } },
-      limit: 1000,
+      sort: { fields: [frontmatter___date], order: DESC },
+      limit: 2000,
     ) {
       edges {
         node {
+          excerpt
           timeToRead
           frontmatter {
             title
-            date
             tags
+            date
             draft
             image {
               childImageSharp {
@@ -224,7 +332,6 @@ export const pageQuery = graphql`
               }
             }
           }
-          excerpt
           fields {
             layout
             slug
